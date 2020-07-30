46f3caae7cb86e3500217db7a1c089abea90dbfc
Joshua <psyjr4@nottingham.ac.uk>
2020-07-30 20:29:54 +0100

    Updated read write units

--- WordServiceExistenceProcessor/test/WordServiceExistenceProcessor.Tests/FunctionTest.cs
+++ WordServiceExistenceProcessor/test/WordServiceExistenceProcessor.Tests/FunctionTest.cs
@@ -1,20 +1,21 @@
-using Amazon.Lambda.TestUtilities;
-using Xunit;
-using FluentAssertions;
-namespace WordServiceExistenceProcessor.Tests
-{
-    public class FunctionTest
-    {
-        [Fact]
-        public void TestToUpperFunction()
-        {
-
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new WordServiceExistenceProcessor.Function();
-            var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
-
-            upperCase.Should().Be("HELLO WORLD");
-        }
-    }
-}
+// using System.Threading.Tasks;
+// using Amazon.Lambda.TestUtilities;
+// using Xunit;
+// using FluentAssertions;
+// namespace WordServiceExistenceProcessor.Tests
+// {
+//     public class FunctionTest
+//     {
+//         [Fact]
+//         public async Task TestToUpperFunction()
+//         {
+//
+//             // Invoke the lambda function and confirm the string was upper cased.
+//             var function = new Function();
+//             var context = new TestLambdaContext();
+//             var upperCase = await function.FunctionHandler("hello world", context);
+//
+//             upperCase.Should().Be("HELLO WORLD");
+//         }
+//     }
+// }
--- WordServiceExistenceProcessor/test/WordServiceExistenceProcessor.Tests/WordServiceTests/WordServiceTests.cs
+++ WordServiceExistenceProcessor/test/WordServiceExistenceProcessor.Tests/WordServiceTests/WordServiceTests.cs
@@ -1,7 +1,35 @@
-﻿namespace WordServiceExistenceProcessor.Tests.WordServiceTests
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Xunit;
+using FluentAssertions;
+using NSubstitute;
+
+namespace WordServiceExistenceProcessor.Tests.WordServiceTests
 {
     public class WordServiceTests
     {
-        
+        [Fact]
+        public async Task TestToUpperFunction()
+        {
+            var dynamoDb = Substitute.For<IAmazonDynamoDB>();
+
+            // dynamoDb.GetItemAsync(new GetItemRequest
+            // {
+            //     TableName = "WordTable",
+            //     Key = new Dictionary<string, AttributeValue>
+            //     {
+            //         {"Word", new AttributeValue {S = input}}
+            //     }
+            // }).Returns(new GetItemResponse
+            // {
+            //     Item = new Dictionary<string, AttributeValue>()
+            // });
+            //
+            var handler = new Handler(dynamoDb);
+            var upperCase = await handler.Handle("hello world");
+            upperCase.Should().Be("HELLO WORLD");
+        }
     }
 }
